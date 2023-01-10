b3afb13c7dfd181e186180a6fdee30c352c38891
Ivaylo Davidov <ivaylo.davidov@amusnet.com>
2023-01-10 20:14:05 +0200

    Add app GUI

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import * as PIXI from 'pixi.js';
-import { resourceTexture } from './utility';
+import { createPanel, resourceTexture } from './utility';
 
 init();
 
@@ -14,10 +14,35 @@ async function init() {
     document.body.appendChild(app.view as HTMLCanvasElement);
     app.ticker.add(update);
 
-    const hoverTexture = await resourceTexture('assets/hover.png',25,105,25,105);
-    const bevelTexture = await resourceTexture('assets/bevel.png',25,105,25,105);
-    const insetTexture = await resourceTexture('assets/inset.png',25,105,25,105);
-  
+    //textures
+    const hoverTexture = await resourceTexture('assets/hover.png', 25, 105, 25, 105);
+    const bevelTexture = await resourceTexture('assets/bevel.png', 25, 105, 25, 105);
+    const insetTexture = await resourceTexture('assets/inset.png', 25, 105, 25, 105);
+
+    //containers
+    const textOutputContainer = new PIXI.Container();
+    const textInputContainer = new PIXI.Container();
+    const buttonContainer = new PIXI.Container();
+
+    //Areas
+    const outputArea = await createPanel(insetTexture, 750, 475);
+    const inputArea = await createPanel(hoverTexture, 575, 50);
+
+    //buttons
+    const orangeButton = await createPanel(hoverTexture, 150, 50);
+    const blueButton = await createPanel(bevelTexture, 150, 50);
+
+    //positioning
+    textOutputContainer.position.set(25,25);
+    textInputContainer.position.set(25,525);
+    buttonContainer.position.set(625,525);
+
+    textOutputContainer.addChild(outputArea);
+    textInputContainer.addChild(inputArea);
+    buttonContainer.addChild(orangeButton,blueButton);
+
+    app.stage.addChild(textOutputContainer,textInputContainer,buttonContainer);
+
     function update() {
 
     }
